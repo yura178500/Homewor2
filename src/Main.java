fb41a8def679184aa20f3aeb772a47bfc65644a4
yura178500 <yura178500@yandex.ru>
2022-10-04 15:43:12 +0300

    Задачи 1-3

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+         //Задача1
+        var dog = 8;
+        System.out.println(dog);
+       var cat = 3.5;
+        System.out.print(cat);
+        var paper = 763789;
+        System.out.print(paper);
+
+        //Задача 2
+        var dog = 8;
+        System.out.println(dog);
+        dog = dog + 4
+        var cat = 3.5;
+        System.out.print(cat);
+        cat = cat + 4
+        var paper = 763789;
+        System.out.print(paper);
+        paper - paper + 4;
+
+        //Задача 3
+        var dog = 8;
+        System.out.println(dog);
+        dog = dog - 3.5
+        var cat = 3.5;
+        System.out.print(cat);
+        cat = cat - 1.6
+        var paper = 763789;
+        System.out.print(paper);
+        paper - paper - 7639;
+    }
+}
